82a3562fd670e7f853bfc09ad0af758bc0e27df3
Nicolás Salas <nsalas@fastmail.com>
2022-07-04 00:10:06 -0400

    feat: loop every 30 secs to set root
    In the future this should be done with threads, but 30 secs for now is
    ok. The most critical feature here is the time

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use chrono::prelude::*;
 use log::info;
 use simple_logger::SimpleLogger;
 use std::process::Command;
+use std::{thread, time};
 
 fn datetime() -> String {
     let local = Local::now();
@@ -21,14 +22,22 @@ fn final_bar() -> String {
     )
 }
 
-fn main() {
-    SimpleLogger::new().init().unwrap();
-
-    info!("starting achbar");
-
+fn set_root() {
     Command::new("xsetroot")
         .arg("-name")
         .arg(final_bar())
         .spawn()
         .expect("Failed to set x root");
+}
+
+fn main() {
+    SimpleLogger::new().init().unwrap();
+
+    info!("starting achbar");
+
+    loop {
+        set_root();
+
+        thread::sleep(time::Duration::from_secs(30));
+    }
 }

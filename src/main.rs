1148d3aca972c4e493a6749a327f78499bdcbefd
Nicolás Salas <nsalas@fastmail.com>
2022-07-04 23:32:49 -0400

    feat: use threads to fetch relevant values

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ use chrono::prelude::*;
 use log::info;
 use simple_logger::SimpleLogger;
 use std::process::Command;
+use std::sync::mpsc;
 use std::{thread, time};
+use linked_hash_map::LinkedHashMap;
 
 fn datetime() -> String {
     let local = Local::now();
@@ -13,19 +15,16 @@ fn datetime() -> String {
     local.format("%a %d %b %H:%M CLST").to_string()
 }
 
-fn final_bar() -> String {
-    format!(
-        "{} | {} | {}",
-        volume::volume().or(Some("".to_string())).unwrap(),
-        bluetooth::devices(),
-        datetime()
-    )
-}
+fn reprint_bar(bar: &LinkedHashMap<&str, String>) {
+    let fmt = bar
+        .values()
+        .map(|str| str.to_string())
+        .collect::<Vec<_>>()
+        .join(" | ");
 
-fn set_root() {
     Command::new("xsetroot")
         .arg("-name")
-        .arg(final_bar())
+        .arg(fmt)
         .spawn()
         .expect("Failed to set x root");
 }
@@ -35,9 +34,41 @@ fn main() {
 
     info!("starting achbar");
 
-    loop {
-        set_root();
+    let mut bar: LinkedHashMap<&str, String> = LinkedHashMap::new();
 
-        thread::sleep(time::Duration::from_secs(30));
+    bar.insert("volume", volume::volume().or(Some(String::from(""))).unwrap());
+    bar.insert("bluetooth", bluetooth::devices());
+    bar.insert("datetime", datetime());
+
+    let (tx, rx): (mpsc::Sender<(&str, String)>, mpsc::Receiver<(&str, String)>) = mpsc::channel();
+
+    let volume_tx = tx.clone();
+    let datetime_tx = tx.clone();
+
+    thread::spawn(move || {
+        loop {
+            let vol = volume::volume().or(Some(String::from(""))).unwrap();
+            volume_tx.send(("volume", vol)).ok();
+
+            thread::sleep(time::Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        loop {
+            datetime_tx.send(("datetime", datetime())).ok();
+
+            thread::sleep(time::Duration::from_secs(30));
+        }
+    });
+
+    loop {
+        let message = rx.recv();
+
+        if let Ok((module, value)) = message {
+            *bar.get_mut(&module).unwrap() = value;
+
+            reprint_bar(&bar);
+        }
     }
 }
